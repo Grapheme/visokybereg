1ae62eba8c021e8617d9f54821391f56efe9fc98
Владимир <vkharseev@gmail.com>
2015-04-06 18:45:18 +0300

    шаблоны

--- app/views/templates/site/plan.blade.php
+++ app/views/templates/site/plan.blade.php
@@ -10,7 +10,6 @@
 @section('page_class')scheme
 @stop
 @section('content')
-
     <div class="content">
         <div class="holder col-2 wside">
             <div class="row">
@@ -25,28 +24,31 @@
                     <div class="unit">
                         <div class="title">Таунхаусы</div><strong>{{ $page->block('townhouses') }}</strong>
                     </div>
-                    <div class="unit"><a href="" class="download-file"><img src="{{ asset(Config::get('site.theme_path').'/images/ico-pdf.png') }}" class="ico">
+                    <div class="unit">
+                        <a href="javascript:void(0);" class="download-file">
+                            <img src="{{ asset(Config::get('site.theme_path').'/images/ico-pdf.png') }}" class="ico">
                             <div class="info">
                                 <div class="filename">Генеральный план.pdf</div>
                                 <div class="size">12мб</div>
-                            </div></a></div>
+                            </div>
+                        </a>
+                    </div>
                 </div>
             </div>
-            <div class="row wide"><img src="{{ asset(Config::get('site.theme_path').'/images/visual-map-scheme.png') }}" class="visual map"></div>
+            <div class="row wide">
+                <img src="{{ asset(Config::get('site.theme_path').'/images/visual-map-scheme.png') }}" class="visual map">
+            </div>
             <div class="row">
                 <div class="text">
-                    <h1>Первая очередь</h1>
-                    <p>
-                        В течении 2015 года будет возведена первая очередь с большим выбором проектов.
-                        Это одноэтажные и двухэтажные таунхаусы с различной жилой площадью в едином архитектурном стиле.
-                    </p>
+                    <h2>{{ $page->block('first_turn') }}</h2>
+                    {{ $page->block('first_turn_text') }}
                 </div>
             </div>
             <div class="row scheme">
-                <div class="visual"><img src="/images/visual-map-scheme-interactive.png"></div>
+                <div class="visual"><img src="{{ asset(Config::get('site.theme_path').'/images/visual-map-scheme-interactive.png') }}"></div>
                 <div class="side">
                     <div class="unit">
-                        <div class="head"><img src="/images/ico-house-1.png" class="ico">
+                        <div class="head"><img src="{{ asset(Config::get('site.theme_path').'/images/ico-house-1.png') }}" class="ico">
                             <div class="title">1 Этажные</div>
                         </div>
                         <ul class="section">
@@ -56,7 +58,7 @@
                         </ul>
                     </div>
                     <div class="unit">
-                        <div class="head"><img src="/images/ico-house-2.png" class="ico">
+                        <div class="head"><img src="{{ asset(Config::get('site.theme_path').'/images/ico-house-2.png') }}" class="ico">
                             <div class="title">2 Этажные</div>
                         </div>
                         <ul class="section">
@@ -65,64 +67,6 @@
                         </ul>
                     </div>
                 </div>
-            </div>
-        </div>
-    </div>
-
-
-    <div class="content">
-        <div class="big-visual">
-            <div class="holder">
-                <h1>{{ $page->seo->h1 }}</h1>
-                <div class="description">
-                    {{ $page->block('description') }}
-                </div>
-            </div>
-        </div>
-        <div class="holder col-2 chess">
-            <div class="row"><img src="{{ asset(Config::get('site.theme_path').'/images/visual-map.png') }}" class="visual map">
-                <div class="text">
-                    <h2>{{ $page->block('location') }}</h2>
-                    {{ $page->block('location_text') }}
-                </div>
-            </div>
-            <div class="row">
-                <div class="text">
-                    <h2>{{ $page->block('lifestyle') }}</h2>
-                    {{ $page->block('lifestyle_text') }}
-                </div>
-                <div class="icons-title-holder">
-                    <div class="unit"><img src="{{ asset(Config::get('site.theme_path').'/images/ico-ball.png') }}" class="ico">
-                        <div class="title">Спортивные площадки</div>
-                    </div>
-                    <div class="unit"><img src="{{ asset(Config::get('site.theme_path').'/images/yacht.png') }}" class="ico">
-                        <div class="title">Яхт-клуб</div>
-                    </div>
-                    <div class="unit"><img src="{{ asset(Config::get('site.theme_path').'/images/ico-cart.png') }}" class="ico">
-                        <div class="title">Супермаркет</div>
-                    </div>
-                    <div class="unit"><img src="{{ asset(Config::get('site.theme_path').'/images/ico-baby.png') }}" class="ico">
-                        <div class="title">Детский сад</div>
-                    </div>
-                    <div class="unit"><img src="{{ asset(Config::get('site.theme_path').'/images/ico-coast.png') }}" class="ico">
-                        <div class="title">Собственный пляж</div>
-                    </div>
-                    <div class="unit"><img src="{{ asset(Config::get('site.theme_path').'/images/ico-book.png') }}" class="ico">
-                        <div class="title">Школа</div>
-                    </div>
-                </div>
-            </div>
-            <div class="row"><img src="{{ asset(Config::get('site.theme_path').'/images/visual-nature-1.png') }}" class="visual">
-                <div class="text">
-                    <h2>{{ $page->block('ecology') }}</h2>
-                    {{ $page->block('ecology_text') }}
-                </div>
-            </div>
-            <div class="row">
-                <div class="text">
-                    <h2>{{ $page->block('ecology_podvorya') }}</h2>
-                    {{ $page->block('ecology_podvorya_text') }}
-                </div><img src="{{ asset(Config::get('site.theme_path').'/images/visual-nature-2.png') }}" class="visual">
             </div>
             <div class="row">
                 <div class="text">
---
+++ app/views/templates/site/projects.blade.php
@@ -0,0 +1,37 @@
+<?
+/**
+ * TITLE: Список проектов
+ * AVAILABLE_ONLY_IN_ADVANCED_MODE
+ */
+?>
+@extends(Helper::layout())
+@section('style')
+@stop
+@section('page_class')project-list
+@stop
+@section('content')
+    <div class="content square-list">
+        <div class="holder">
+            <h1>{{ $page->seo->h1 }}</h1>
+            <a href="" class="unit">
+                <div style="background-image:url('http://dummyimage.com/497x305/');" class="visual"></div>
+                <div class="info">
+                    <div class="small">67м<sup>2</sup></div>
+                    <div class="big">
+                        <div class="wrapper">
+                            <h2>Одноэтажный таунхаус, 67м<sup>2</sup></h2>
+                            <div class="info">от 35 000 руб. за м<sup>2</sup></div>
+                        </div>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="row">
+            <div class="text">
+                {{ $page->block('seo') }}
+            </div>
+        </div>
+    </div>
+@stop
+@section('scripts')
+@stop
